e0f406cd0711124fd7bd0e429f6b6b5e16f2e050
Diveloment <91672317+Diveloment@users.noreply.github.com>
2022-12-11 19:36:44 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entities.DemoDBEntities Context = new Entities.DemoDBEntities();
+        public static Entities.Users authUser = null; //0 - guest; 1 - admin; 2 - client; 3 - manager
+        public static bool CheckRights(int role)
+        {
+            if (authUser.UserRoles.UserRole_id == role)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
